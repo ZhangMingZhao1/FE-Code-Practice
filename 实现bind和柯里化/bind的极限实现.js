b763a6f602dcc47c228cbf64794558e9dd31e23b
ZhangMingZhao1 <lawliet_zmz@foxmail.com>
2018-03-27 23:22:08 +0800

    bind的实现，考虑偏函数和构造使用问题

---
+++ 实现bind和柯里化/bind的极限实现.js
@@ -0,0 +1,40 @@
+/*
+
+函数体内的this，就是需要绑定this的实例函数，或者说是原函数。最后我们使用apply来进行参数（context）绑定，并返回。
+同时，将第一个参数（context）以外的其他参数，作为提供给原函数的预设参数，这也是基本的柯里化基础。
+
+*/
+Function.prototype.bind =  Function.prototype.bind || function(context) {
+    var self = this;
+    var arg = Array.prototype.slice.call(arguments,1);
+    return function() {
+        return self.apply(context,arr);
+    }
+}
+
+Function.prototype.bind =  Function.prototype.bind || function(context) {
+    var self = this;
+    var args = Array.prototype.slice.call(arguments,1);
+    return function() {
+        var innerArgs = Array.prototype.slice.call(arguments);
+        var FinalArgs = args.concat(innerArgs);
+        return self.apply(context,FinalArgs);
+    }
+}
+
+Function.prototype.bind =  Function.prototype.bind || function(context) {
+    var self = this;
+    var args = Array.prototype.slice.call(arguments,1);
+    var Fun = {};
+    Fun.prototype = this.prototype;//继承原来函数
+    var Comb = function(){
+        var innerArgs = Array.prototype.slice.call(arguments);
+        var FinalArgs = args.concat(innerArgs);
+        return self.apply(this instanceof Fun ? this : context || this ,FinalArgs);
+    }
+    Comb.prototype = new Fun();
+    return Comb;
+    }
+    
+
+
