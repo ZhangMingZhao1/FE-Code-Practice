d2b20bb5c2c120a5be79ed5d3b0d3909856e096e
张明朝 <1104272319@qq.com>
2018-08-29 21:05:41 +0800

    prmise控制读取文件

--- Pomise interview/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-function readFile(fileName) {
-    return new Promise(function(resolve,reject) {
-        fs.readFile("in.txt",)
-    })
-}
---
+++ Pomise控制读取文件/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+function readFile(fileName) {
+    return new Promise(function(resolve,reject) {
+        fs.readFile("in.txt",{encoding:"utf8"},function(err,data) {
+            resolve(data)
+        })
+    })
+}
+
+let p = readFile("in.txt")
+p.then(data=>{console.log(data)}).catch((err)=>console.log(err))
