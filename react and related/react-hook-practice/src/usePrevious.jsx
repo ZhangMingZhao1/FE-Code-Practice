53e4f6db6831c3634278654ea4dcbb9209c57426
ZhangMingZhao1 <1104272319@qq.com>
2020-03-16 12:04:06 +0800

    feat: usePrevious解释修改

--- react and related/react-hook-practice/src/usePrevious.jsx
+++ react and related/react-hook-practice/src/usePrevious.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react"
 
 function App() {
   //   before useEffect
   //   after useEffect
   //   useEffect
-  // useEffect 一直是render之后执行的，所以在render中的ref值总是落后一次
-  let pre = useRef();
-  const [state, setState] = useState(0);
-  console.log("before useEffect");
+  // useEffect 一直是render之后执行的，相当于Did的生命周期，对标DidMount和DidUpdate，先render渲染，再执行Did 所以在我们先看到的是render的未赋值的ref.current, 后被赋值。
+  let pre = useRef()
+  const [state, setState] = useState(0)
+  console.log("before useEffect")
   useEffect(() => {
-    console.log("useEffect");
-    pre.current = state;
-  });
-  console.log("after useEffect");
+    console.log("useEffect")
+    pre.current = state
+  })
+  console.log("after useEffect")
 
   return (
     <div onClick={() => setState(state + 1)}>
@@ -21,7 +21,7 @@ function App() {
       {"   "}
       <div>cur:{state}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
