e8dce44e148bb82d8eb96c7f8325b27112c41399
ZhangMingZhao1 <1104272319@qq.com>
2020-05-15 18:08:19 +0800

    feat: Infinite scrolling solution

--- Interview Questions/Promise实现红绿灯轮询打印/1.js
+++ Interview Questions/Promise实现红绿灯轮询打印/1.js
@@ -1,1 +1,10 @@
-let settimeout = (name,time)=> return new Promise(()=>{})
+let timeout = (name, ms) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(name);
+    }, ms);
+  });
+
+let r = timeout("红灯", 1000);
+let g = timeout("绿灯", 1000);
+let b = timeout("蓝灯", 1000);
