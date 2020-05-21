c75bcdf95d501e1f63de31fd78a1d1b2a4f04c8c
ZhangMingZhao1 <1104272319@qq.com>
2020-05-21 17:53:59 +0800

    feat: 定义一个promise类型

---
+++ TypeScript/定义一个Promise类型/1.ts
@@ -0,0 +1,19 @@
+/*
+  Promise在npm里面已定义过，resolve(value)value的值即是Promise<T> T的值
+  一般我们封装请求方法，对axios的返回值定义类型
+*/
+export type Response<T> = {
+  [P in keyof T]: T[P];
+} & { code: number | string };
+
+function request(): Promise<Response<T>> {
+  return new Promise(
+    (reslove) => {
+      // async do something
+      reslove({ code: 200, data: "ddd" });
+    }
+    // (reject) => {
+    //   reject("11");
+    // }
+  );
+}
