deaaa88aa7561d4cb51c0f16ad481d215838afce
ZhangMingZhao1 <1104272319@qq.com>
2020-05-27 23:07:16 +0800

    feat: 补充bind实现

--- Native JavaScript/实现bind和柯里化/bind的极限实现.js
+++ Native JavaScript/实现bind和柯里化/bind的极限实现.js
@@ -4,37 +4,43 @@
 同时，将第一个参数（context）以外的其他参数，作为提供给原函数的预设参数，这也是基本的柯里化基础。
 
 */
-Function.prototype.bind =  Function.prototype.bind || function(context) {
+Function.prototype.bind =
+  Function.prototype.bind ||
+  function (context) {
     var self = this;
-    var arg = Array.prototype.slice.call(arguments,1);
-    return function() {
-        return self.apply(context,arr);
-    }
-}
+    var arg = Array.prototype.slice.call(arguments, 1);
+    return function () {
+      return self.apply(context, arg);
+    };
+  };
 
-Function.prototype.bind =  Function.prototype.bind || function(context) {
+Function.prototype.bind =
+  Function.prototype.bind ||
+  function (context) {
     var self = this;
-    var args = Array.prototype.slice.call(arguments,1);
-    return function() {
-        var innerArgs = Array.prototype.slice.call(arguments);
-        var FinalArgs = args.concat(innerArgs);
-        return self.apply(context,FinalArgs);
-    }
-}
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function () {
+      var innerArgs = Array.prototype.slice.call(arguments);
+      var FinalArgs = args.concat(innerArgs);
+      return self.apply(context, FinalArgs);
+    };
+  };
 
-Function.prototype.bind =  Function.prototype.bind || function(context) {
+Function.prototype.bind =
+  Function.prototype.bind ||
+  function (context) {
     var self = this;
-    var args = Array.prototype.slice.call(arguments,1);
+    var args = Array.prototype.slice.call(arguments, 1);
     var Fun = {};
-    Fun.prototype = this.prototype;//继承原来函数
-    var Comb = function(){
-        var innerArgs = Array.prototype.slice.call(arguments);
-        var FinalArgs = args.concat(innerArgs);
-        return self.apply(this instanceof Fun ? this : context || this ,FinalArgs);
-    }
+    Fun.prototype = this.prototype; //继承原来函数
+    var Comb = function () {
+      var innerArgs = Array.prototype.slice.call(arguments);
+      var FinalArgs = args.concat(innerArgs);
+      return self.apply(
+        this instanceof Fun ? this : context || this,
+        FinalArgs
+      );
+    };
     Comb.prototype = new Fun();
     return Comb;
-    }
-    
-
-
+  };
