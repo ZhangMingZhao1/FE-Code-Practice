a8b99fed0d241233baeb43d53a6d7a1923016558
ZhangMingZhao1 <1104272319@qq.com>
2020-05-27 19:07:15 +0800

    feat:定义一个promise类型补充2

--- TypeScript/定义一个Promise类型/1.ts
+++ TypeScript/定义一个Promise类型/1.ts
@@ -18,6 +18,22 @@ function request(): Promise<Response<T>> {
   );
 }
 
+// 泛型接口定义普通函数
+interface CreateArrayFunc {
+  <T>(length: number, value: T): Array<T>;
+}
+
+let createArray: CreateArrayFunc;
+createArray = function <T>(length: number, value: T): Array<T> {
+  let result: T[] = [];
+  for (let i = 0; i < length; i++) {
+    result[i] = value;
+  }
+  return result;
+};
+
+createArray(3, "x"); // ['x', 'x', 'x']
+
 interface AxiosInstance {
   <T = any>(value: T): Promise<T>;
 }
