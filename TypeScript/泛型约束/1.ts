3fca5e2d44070cef2e198a8cdd8370cb0d2ecdfb
ZhangMingZhao1 <1104272319@qq.com>
2020-05-27 18:45:36 +0800

    feat: 泛型约束

---
+++ TypeScript/泛型约束/1.ts
@@ -0,0 +1,8 @@
+interface Lengthwise {
+  length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+  console.log(arg.length); // Now we know it has a .length property, so no more error
+  return arg;
+}
