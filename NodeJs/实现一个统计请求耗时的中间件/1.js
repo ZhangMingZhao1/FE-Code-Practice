0212d20b31baec2b6d7587eabd9a185862c9c75f
ZhangMingZhao1 <1104272319@qq.com>
2020-05-27 23:18:15 +0800

    feat: 补充实现一个统计请求耗时的中间件

--- NodeJs/实现一个统计请求耗时的中间件/1.js
+++ NodeJs/实现一个统计请求耗时的中间件/1.js
@@ -1,7 +1,7 @@
-exports.responseTime = function() {
-  return function(req, res, next) {
+exports.responseTime = function () {
+  return function (req, res, next) {
     req._startTime = new Date();
-    let calResponseTime = function() {
+    let calResponseTime = function () {
       var now = new Date();
       var lastTime = now - req._startTime;
       console.log(lastTime);
@@ -11,3 +11,27 @@ exports.responseTime = function() {
     return next();
   };
 };
+
+// 另外一种思路，koa的
+const Koa = require("koa");
+const app = new Koa();
+
+// logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.method} ${ctx.url} ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+// response
+app.use(async (ctx) => {
+  ctx.body = "Hello World";
+});
