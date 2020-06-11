e5dd52c18afec2245ca22577ebd6ce9323243d5b
ZhangMingZhao1 <1104272319@qq.com>
2020-06-11 19:31:52 +0800

    feat: update 最大子序列和

--- Data structures and algorithms/最大子序列和/1.js
+++ Data structures and algorithms/最大子序列和/1.js
@@ -2,18 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
   if (nums.length === 1 && nums[0] <= 0) return nums[0];
   let sum = 0;
   let max = -99999999;
-  let cnt = 1;
+
   for (let i of nums) {
     sum += i;
     if (sum > max) {
       max = sum;
     }
     if (sum < 0) sum = 0;
-    cnt++;
   }
   return max;
 };
