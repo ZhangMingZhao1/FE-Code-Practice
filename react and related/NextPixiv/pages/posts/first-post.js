7e2246c71d87207f9d58e84aa7c9870b8d7b1117
ZhangMingZhao1 <1104272319@qq.com>
2020-06-16 22:57:32 +0800

    feat: nextjs demo

---
+++ react and related/NextPixiv/pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function FirstPost() {
+  return (
+    <>
+      <Link href="/">
+        <a>back to home</a>
+      </Link>
+      <h1>First Post</h1>
+    </>
+  );
+}
